29d0bbed88597b17cea8af5b99a777595382b21a
Benjamin Metaut <metautbenjamin@hotmail.fr>
2018-11-03 16:18:54 +0100

    add three type of movie list

--- app/src/main/java/fr/esgi/moc/moviemeeting/movies/MoviesListAdapter.java
+++ app/src/main/java/fr/esgi/moc/moviemeeting/movies/MoviesListAdapter.java
@@ -2,85 +2,88 @@ package fr.esgi.moc.moviemeeting.movies;
 
 import android.content.Context;
 
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import fr.esgi.moc.moviemeeting.R;
 import fr.esgi.moc.moviemeeting.data.dtos.Movie;
 
-public class MoviesListAdapter extends BaseAdapter {
+public class MoviesListAdapter extends RecyclerView.Adapter<MoviesListAdapter.MyViewHolder> {
     private Context context;
     private List<Movie> movies;
     private final String BASEIMG_URL = "http://image.tmdb.org/t/p/w185/";
+    private static final String TAG = "MoviesListAdapter";
 
 
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.movie_poster)
+        ImageView moviePoster;
+        @BindView(R.id.movie_title)
+        TextView movieTitle;
+        @BindView(R.id.movie_author)
+        TextView movieAuthor;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            Log.d(TAG, "view holder called");
+        }
+    }
 
     public MoviesListAdapter(Context context, List<Movie> movies) {
         this.context = context;
         this.movies = movies;
     }
 
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-        View gridView;
-
-        if (convertView == null) {
-
-            gridView = new View(context);
-
-            // get layout from mobile.xml
-            gridView = inflater.inflate(R.layout.cell_movies, null);
-
-            // set image based on selected text
-            ImageView imageView = (ImageView) gridView
-                    .findViewById(R.id.movie_poster);
-
-
-            if(movies.get(position) != null){
-                //imageView.setImageResource(mThumbIds[position]);
-                //Log.i("image url : ", BASEIMG_URL + movies.get(position).getPoster_path());
-                String imgUrl = BASEIMG_URL + movies.get(position).getPoster_path();
-                Picasso.get().load(imgUrl).into(imageView);
-
-            }
-
-
-        } else {
-            gridView = (View) convertView;
-        }
-
-        return gridView;
-    }
 
     public void updateData(Movie movie) {
 
         this.movies.add(movie);
     }
 
-    @Override
-    public int getCount() {
-        return movies.size();
-    }
 
-    @Override
     public Object getItem(int position) {
         return null;
     }
 
     @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cell_movies, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        final Movie movie = movies.get(position);
+
+
+        String imgUrl = BASEIMG_URL + movie.getPoster_path();
+        Picasso.get().load(imgUrl).into(holder.moviePoster);
+        holder.movieTitle.setText(movie.getTitle());
+
+    }
+
+    @Override
     public long getItemId(int position) {
         return 0;
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
     }
 
 }
